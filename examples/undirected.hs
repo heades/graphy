71d02e5dffa53533f56e50b5c66c2f05cb272944
Harley Eades <heades@gru.edu>
2016-04-21 09:06:03 -0400

    Small change.

--- examples/undirected.hs
+++ examples/undirected.hs
@@ -1,8 +1,8 @@
 import Graphy
 
-qGraph :: Graph Char ()
-qGraph = [('a' , 'c', ()), ('b', 'c', ()), ('c', 'd', ()),
-          ('d', 'b', ()),    ('a', 'b', ()),    ('e', 'a', ()),
-          ('e', 'd', ()),    ('a', 'a', ())]   
+graph :: Graph Char ()
+graph = [('a' , 'c', ()), ('b', 'c', ()), ('c', 'd', ()),
+         ('d', 'b', ()),  ('a', 'b', ()), ('e', 'a', ()),
+         ('e', 'd', ()),  ('a', 'a', ())]   
 
-main = mkImage Dot Jpeg 0.23 0.3 2 qGraph "ex-graph.jpeg"
+main = mkImage Dot Jpeg 0.23 0.3 2 graph "ex-graph.jpeg"
